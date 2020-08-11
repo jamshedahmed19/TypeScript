44260791ce8cae151495f0655491c9a9a0a146a6
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2020-08-11 18:16:22 +0500

    The DOM & Type Casting

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,34 @@
 "use strict";
-//in Typscript we can predefine a functions structure
-var calc;
-calc = function (n1, n2, action) {
-    if (action === 'add') {
-        return n1 + n2;
-    }
-    else {
-        return n1 - n2;
-    }
-};
-console.log(calc(2, 10, 'add'));
-var user;
-user = function (person) {
-    console.log(person.name + " is " + person.age + " years old");
-};
-user({ name: 'jamshed', age: 19 });
+//when we select element in Typscript it gives an error if
+//we try to access any property of that element
+// const anchor = document.querySelector('a');
+// console.log(anchor);
+// console.log(anchor.href);
+//because Typescript don't know whether that element exist
+//so we can over come this problem by using conditional statment or
+//by adding exclamation mark
+// const anchor = document.querySelector("a");
+// if (anchor) {
+//   console.log(anchor.href);
+// }
+var anchor = document.querySelector("a");
+console.log(anchor);
+console.log(anchor.href);
+//Typscript knows the element type if we hover over anchor it
+//show its elemet type it is usefull because then if we use that
+//anchor tag it will only give those properties and methods related to it
+console.log(anchor.tagName);
+//if we select an element by class name then ts can't know element type of that class
+//so we can type cast it by ourselves by using as keyword and then we don't to use ! sign
+//const form = document.querySelector('form');
+var form = document.querySelector(".new-item-form");
+console.log(form.children);
+//selecting inputs
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,18 +1,40 @@
-//in Typscript we can predefine a functions structure
-let calc: (a: number, b: number, action: string) => number;
-calc = (n1: number, n2: number, action: string) => {
-    if(action === 'add'){
-        return n1 + n2;
-    }else{
-        return n1 - n2;
-    }
-}
-console.log(calc(2,10,'add'));
+//when we select element in Typscript it gives an error if
+//we try to access any property of that element
 
-//ex#02
-type personDetails = {name: string, age: number | string};
-let user: (person: personDetails) => void;
-user = (person: personDetails) => {
-    console.log(`${person.name} is ${person.age} years old`)
-}
-user({name:'jamshed',age: 19});
+// const anchor = document.querySelector('a');
+// console.log(anchor);
+// console.log(anchor.href);
+
+//because Typescript don't know whether that element exist
+//so we can over come this problem by using conditional statment or
+//by adding exclamation mark
+// const anchor = document.querySelector("a");
+// if (anchor) {
+//   console.log(anchor.href);
+// }
+
+const anchor = document.querySelector("a")!;
+console.log(anchor);
+console.log(anchor.href);
+
+//Typscript knows the element type if we hover over anchor it
+//show its elemet type it is usefull because then if we use that
+//anchor tag it will only give those properties and methods related to it
+console.log(anchor.tagName);
+
+//if we select an element by class name then ts can't know element type of that class
+//so we can type cast it by ourselves by using as keyword and then we don't to use ! sign
+//const form = document.querySelector('form');
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+console.log(form.children);
+
+//selecting inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
